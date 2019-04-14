83c65d9384024ffac8ada29e874e668958eb79d4
nguoianphu <nguoianphu@gmail.com>
2019-04-14 08:00:03 +0000

    Flappy-bird works

--- src/games/flappy-bird/game.ts
+++ src/games/flappy-bird/game.ts
@@ -5,44 +5,48 @@
  * @license      Digitsensitive
  */
 
-import "phaser";
-import { BootScene } from "./scenes/bootScene";
-import { MainMenuScene } from "./scenes/mainMenuScene";
-import { GameScene } from "./scenes/gameScene";
+import 'phaser';
+import { BootScene } from './scenes/bootScene';
+import { MainMenuScene } from './scenes/mainMenuScene';
+import { GameScene } from './scenes/gameScene';
 
 const config: GameConfig = {
-  title: "Flappy Bird",
-  url: "https://github.com/digitsensitive/phaser3-typescript",
-  version: "1.0",
-  width: 405,
-  height: 600,
-  zoom: 1,
-  type: Phaser.AUTO,
-  parent: "game",
-  scene: [BootScene, MainMenuScene, GameScene],
-  input: {
-    keyboard: true,
-    mouse: false,
-    touch: false,
-    gamepad: false
-  },
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: 300 },
-      debug: false
+    title: 'Flappy Bird',
+    url: 'https://github.com/digitsensitive/phaser3-typescript',
+    version: '1.0',
+    type: Phaser.AUTO,
+    scene: [BootScene, MainMenuScene, GameScene],
+    input: {
+        keyboard: true,
+        mouse: false,
+        touch: false,
+        gamepad: false
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    backgroundColor: '#98d687',
+    render: { pixelArt: true, antialias: false },
+    scale: {
+        width: 405,
+        height: 600,
+        // zoom: 1,
+        parent: 'game',
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
     }
-  },
-  backgroundColor: "#98d687",
-  render: { pixelArt: true, antialias: false }
 };
 
 export class Game extends Phaser.Game {
-  constructor(config: GameConfig) {
-    super(config);
-  }
+    constructor(config: GameConfig) {
+        super(config);
+    }
 }
 
-window.addEventListener("load", () => {
-  var game = new Game(config);
+window.addEventListener('load', () => {
+    var game = new Game(config);
 });
