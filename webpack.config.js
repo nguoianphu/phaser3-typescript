711556bba5f9a2e38df934c127a226be47094ae5
nguoianphu <nguoianphu@gmail.com>
2019-04-15 06:46:29 +0000

    Remove all comments when building

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,52 @@
 var path = require('path');
 var pathToPhaser = path.join(__dirname, '/node_modules/phaser/');
 var phaser = path.join(pathToPhaser, 'dist/phaser.js');
+const TerserPlugin = require('terser-webpack-plugin');
+// const JavaScriptObfuscator = require('webpack-obfuscator');
 
 module.exports = {
-  entry: './src/games/flappy-bird/game.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      { test: /\.ts$/, loader: 'ts-loader', exclude: '/node_modules/' },
-      { test: /phaser\.js$/, loader: 'expose-loader?Phaser' }
-    ]
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, './'),
-    publicPath: '/dist/',
-    compress: true,
-    port: 8080,
-    host: '0.0.0.0',
-    disableHostCheck: true,
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-    alias: {
-      phaser: phaser
-    }
-  }
+    entry: './src/games/flappy-bird/game.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, loader: 'ts-loader', exclude: '/node_modules/' },
+            { test: /phaser\.js$/, loader: 'expose-loader?Phaser' }
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, './'),
+        publicPath: '/dist/',
+        compress: true,
+        port: 8080,
+        host: '0.0.0.0',
+        disableHostCheck: true
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+        alias: {
+            phaser: phaser
+        }
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    output: {
+                        comments: false
+                    }
+                }
+            })
+        ]
+    },
+    // plugins: [
+    //     new JavaScriptObfuscator(
+    //         {
+    //             rotateUnicodeArray: true
+    //         },
+    //         env === 'production' ? [] : ['bundle.js'] 
+    //     )
+    // ]
 };
